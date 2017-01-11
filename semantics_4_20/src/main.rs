8c030dcd438cafb9fd17f64e0baa7de01edc0a11
m0riiiii <rewop.ecnalab.x2@gmail.com>
2017-01-11 23:53:24 +0900

    4.20

--- semantics_4_20/src/main.rs
+++ semantics_4_20/src/main.rs
@@ -1,3 +1,24 @@
+struct HasDrop;
+
+impl Drop for HasDrop {
+    fn drop(&mut self) {
+        println!("Dropping!")
+    }
+}
+
+struct Firework {
+    strength: i32,
+}
+
+impl Drop for Firework {
+    fn drop(&mut self) {
+        println!("BOOM times {}!!!",self.strength);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = HasDrop;
+    // dropメソッドはtntから呼ばれる
+    let firecracker = Firework { strength: 1 };
+    let tnt = Firework { strength: 100 };
 }
