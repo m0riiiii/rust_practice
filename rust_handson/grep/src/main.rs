8f45da4fda768d53bac22e4946bc29e13f91155a
m0riiiii <rewop.ecnalab.x2@gmail.com>
2017-01-23 22:29:30 +0900

    grep修正

--- rust_handson/grep/src/main.rs
+++ rust_handson/grep/src/main.rs
@@ -1,9 +1,14 @@
 use std::env;
+use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
-        println!("{:?}", args)
+        let mut file = try!(File::open(args[1]));
+        let mut concat = String::new();
+
+        try!(file.read_to_string(&mut concat));
+        println!("{:?}", concat);
     }
 }
