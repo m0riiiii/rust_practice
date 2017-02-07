dc21c8f7940d43fd16681f303539e254702c0202
m0riiiii <rewop.ecnalab.x2@gmail.com>
2017-02-07 22:35:04 +0900

    grep 更新

--- rust_handson/grep/src/main.rs
+++ rust_handson/grep/src/main.rs
@@ -5,11 +5,17 @@ use std::fs::File;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 {
+    if args.len() > 2 {
         let mut file = File::open(&args[1]).unwrap();
         let mut concat = String::new();
 
         file.read_to_string(&mut concat);
-        println!("{:?}", concat);
+        let strings: Vec<&str> = concat.split('\n').collect();
+
+        for (line_count,line) in strings.iter().enumerate() {
+            if line.contains(&args[2]) {
+                println!("{} Ln.{}: {}", &args[1], line_count, line);
+            }
+        }
     }
 }
