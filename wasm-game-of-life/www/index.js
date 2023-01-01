2a7f16c3082e74aef3035144e3db9c46921adae5
m0riiiii <rewop.ecnalab.x2@gmail.com>
2023-01-01 11:17:06 +0900

    change render ratio

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -3,9 +3,14 @@ import {Universe} from "wasm-game-of-life";
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
 
+const DELAY_RATIO = 8;
+let count = 0;
 const renderLoop = () => {
-    pre.textContent = universe.render();
-    universe.tick();
+    if (count % DELAY_RATIO === 0) {
+        pre.textContent = universe.render();
+        universe.tick();
+    }
+    count++;
 
     requestAnimationFrame(renderLoop);
 }
